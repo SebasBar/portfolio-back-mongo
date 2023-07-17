8ff7088144ee747d7f998fa75de0d391db9f5720
Sebas <toolt2y@gmail.com>
2023-07-17 19:09:55 +0200

    serving frontend on port 3001

--- app.js
+++ app.js
@@ -3,15 +3,25 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongodbURL = process.env.MONGOOSEDB_URL;
+const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
-const projectsRouter = require('./routes/project.routes')
+const projectsRouter = require('./routes/project.routes');
 const cors = require("cors");
+const path = require("path");
 
 //middleware
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/projects', projectsRouter);
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", (req, res) => {
+
+  console.log('request: ' + req);
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 //configure mongoose
 mongoose.connect(
@@ -29,8 +39,8 @@ mongoose.connect(
   }
 );
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
